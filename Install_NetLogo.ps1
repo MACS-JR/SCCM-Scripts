d6b6ae59d470ae097605e9a8a867e0a5afb9e9c4
MACS-JR <jreichel@mexico.cnyric.org>
2014-07-17 14:14:33 -0400

    Misc install scripts

---
+++ Install_NetLogo.ps1
@@ -0,0 +1,21 @@
+﻿#Jeff Reichel - Mexico Academy & Central School - 07.16.2014
+
+Start-Process -FilePath "NetLogo5.0.5Installer.exe" -ArgumentList " -q" -WindowStyle hidden -PassThru -Wait
+
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$_${Env:ProgramData}\Microsoft\Windows\Start Menu\Programs\NetLogo 5.0.5.lnk")
+$Shortcut.TargetPath = "$_${Env:ProgramFiles(x86)}\NetLogo 5.0.5\NetLogo 5.0.5.exe"
+$Shortcut.Arguments = ""
+$Shortcut.IconLocation = "$_${Env:ProgramFiles(x86)}\NetLogo 5.0.5\NetLogo 5.0.5.exe"
+$Shortcut.Description ="NetLogo 5.0.5"
+$Shortcut.WorkingDirectory ="H:"
+$Shortcut.Save()
+
+$Shortcut = $WshShell.CreateShortcut("$_${Env:SYSTEMDRIVE}\Users\Public\Desktop\NetLogo 5.0.5.lnk")
+$Shortcut.TargetPath = "$_${Env:ProgramFiles(x86)}\NetLogo 5.0.5\NetLogo 5.0.5.exe"
+$Shortcut.Arguments = ""
+$Shortcut.IconLocation = "$_${Env:ProgramFiles(x86)}\NetLogo 5.0.5\NetLogo 5.0.5.exe"
+$Shortcut.Description ="NetLogo 5.0.5"
+$Shortcut.WorkingDirectory ="H:"
+$Shortcut.Save()
+
---
+++ install_firessh.ps1
@@ -0,0 +1,33 @@
+﻿#Jeff Reichel - Mexico Academy & Central School - 07.11.2014
+
+Param(
+     [Parameter(Mandatory=$True)]
+     [string[]]$action
+	 )
+	 
+$addon_file = "firessh-0.94.5-fx.xpi"
+$addon_id = "firessh@nightlight.ws"
+$file_source = "\\macssccm01\repository$\Apps\FireFox_Add-on_FireSSH"
+$file_dest = "${env:ProgramFiles(x86)}\Mozilla Firefox\browser\extensions"
+$reg_location = 'HKLM:\SOFTWARE\Wow6432Node\Mozilla\Firefox\Extensions'
+$reg_path = $reg_location -split '\\'
+
+If ($action -eq 'install') {
+	if (-not (Test-Path ("$file_dest\$addon_file"))) {
+		Copy-Item "$file_source\$addon_file" -Destination "$file_dest\"
+	}
+	if (-not (Test-Path ($reg_location))) {
+		New-Item -Path ($reg_path[0..($reg_path.count - 2)] -join '\') -Name "Extensions"
+	}
+	if (-not (Get-Itemproperty -Path $reg_location -Name $addon_id)) {
+		New-ItemProperty -Path $reg_location -Name $addon_id -PropertyType String -Value "$file_dest\$addon_file" -Force
+	}
+}
+elseif ($action -eq 'uninstall') {
+	if (Test-Path ("$file_dest\$addon_file")) {
+		Remove-Item "$file_dest\$addon_file"
+	}
+	if (Get-Itemproperty -Path $reg_location -Name $addon_id) {
+		Remove-ItemProperty -Path $reg_location -Name $addon_id
+	}
+}
